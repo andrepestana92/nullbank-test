272c9fd9d7527432594dceb2e0ef92dc037976f2
Andre Pestana <andre.pest92@gmail.com>
2016-01-25 21:18:02 -0200

    empty graph

--- main.scala
+++ main.scala
@@ -5,7 +5,7 @@ import scalax.collection.GraphPredef._, scalax.collection.GraphEdge._
 
 object CalcPoints {
 	def main(args: Array[String]): Unit = {
-		val g = Graph(3~1, 5)
+		val g = Graph(0~0, 0)
 		for(line <- Source.fromFile(args(0)).getLines()) {
   			val nodes = line.split(" ")
   			implicit val factory = scalax.collection.GraphEdge.UnDiEdge
