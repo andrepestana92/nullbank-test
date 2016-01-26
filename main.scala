ec71584ae0cb75e7203ca7e76e2e4d1dfe6f09e9
Andre Pestana <andre.pest92@gmail.com>
2016-01-26 10:19:57 -0200

    including only people who hasn't been invited yet.

--- main.scala
+++ main.scala
@@ -5,11 +5,18 @@ import scalax.collection.GraphPredef._, scalax.collection.GraphEdge._
 
 object CalcPoints {
 	def main(args: Array[String]): Unit = {
-		val g = Graph(0~0, 0)
+		val g = Graph((-1)~(-1), -1)
 		for(line <- Source.fromFile(args(0)).getLines()) {
   			val nodes = line.split(" ")
-  			implicit val factory = scalax.collection.GraphEdge.UnDiEdge
-  			g.addEdge(nodes(0).toInt,nodes(1).toInt)
+  			val peopleInList = g.nodes mkString " "
+
+  			/*Check if person invited is already in the list. If so,
+  			the inviter won't get the points*/
+  			if (! (peopleInList contains nodes(1))) {
+  				implicit val factory = scalax.collection.GraphEdge.UnDiEdge
+  				g.addEdge(nodes(0).toInt,nodes(1).toInt)
+  			}
   		}
+  		println(g.edges mkString " ")
 	}
 }
