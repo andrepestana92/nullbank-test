707c8c516612b2d43da419a0483993e7bd961d89
Andre Pestana <andre.pest92@gmail.com>
2016-01-27 16:38:26 -0200

    making the right calculation

--- main.scala
+++ main.scala
@@ -1,17 +1,22 @@
 import scala.io.Source
 import scalax.collection.mutable.Graph
 import scalax.collection.GraphPredef._, scalax.collection.GraphEdge._
+import scala.collection.mutable.ListBuffer
 
 
 object CalcPoints {
+  val inviters = new ListBuffer[String]
+
 	def main(args: Array[String]): Unit = {
 		val g = Graph((-1)~>(-1), -1)
+    
 		for(line <- Source.fromFile(args(0)).getLines()) {
   			val nodes = line.split(" ")
   			val peopleInList = g.nodes mkString " "
 
+        inviters += nodes(0)
   			/*Check if person invited is already in the list. If so,
-  			the inviter won't get the points*/
+  			the inviter won't get the points */
   			if (! (peopleInList contains nodes(1))) {
   				implicit val factory = scalax.collection.GraphEdge.DiEdge
   				g.addEdge(nodes(0).toInt,nodes(1).toInt)
@@ -23,6 +28,7 @@ object CalcPoints {
 
       for (person <- allPeople) {
         val points = calcNodeValue(g, person, 0)
+        println(person + " " + points)
       }
 	}
 
@@ -31,7 +37,9 @@ object CalcPoints {
     val node = g get person.toInt
     val childs = node.diSuccessors
     for (child <- childs) {
-      sum += (scala.math.pow( (0.5), weight) + calcNodeValue(g, child.toString, weight + 1))
+      if (inviters contains child.toString) {
+        sum += (scala.math.pow( (0.5), weight) + calcNodeValue(g, child.toString, weight + 1))
+      }
     }
     return sum
   }
