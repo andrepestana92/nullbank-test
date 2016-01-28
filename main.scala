db2ef2811187ccf321b9ca78be84bd14736caff5
Andre Pestana <andre.pest92@gmail.com>
2016-01-28 11:12:24 -0200

    getting new edges via stdin after processing the input file

--- main.scala
+++ main.scala
@@ -6,32 +6,51 @@ import scala.collection.mutable.ListBuffer
 
 object CalcPoints {
   val inviters = new ListBuffer[String]
+  val allPeople = new ListBuffer[String]
   val g = Graph((-1)~>(-1), -1)
 
 	def main(args: Array[String]): Unit = {
 		
     
 		for(line <- Source.fromFile(args(0)).getLines()) {
-  			val nodes = line.split(" ")
-  			val peopleInList = g.nodes mkString " "
-
-        inviters += nodes(0)
-  			/*Check if person invited is already in the list. If so,
-  			the inviter won't get the points */
-  			if (! (peopleInList contains nodes(1))) {
-  				implicit val factory = scalax.collection.GraphEdge.DiEdge
-  				g.addEdge(nodes(0).toInt,nodes(1).toInt)
-  			}
+        registerEdge(line)			
   		}
 
-      g -= (-1)
-      val allPeople = (g.nodes mkString " ").split(" ")
+    g -= (-1)
+    getPoints()
+    for (newEdge <- io.Source.stdin.getLines) {
+      if (newEdge.isEmpty)
+        sys.exit()
 
-      for (person <- allPeople) {
-        val points = calcNodeValue(person, 0)
-        println(person + " " + points)
-      }
+      registerEdge(newEdge)
+      getPoints()
+    }
 	}
+
+  def registerEdge(newEdge: String): Unit = {
+    val nodes = newEdge.split(" ")
+
+    if (! (inviters contains nodes(0)) ) 
+      inviters += nodes(0)
+    /*Check if person invited is already in the list. If so,
+    the inviter won't get the points */
+    if (! (allPeople contains nodes(1))) {
+      implicit val factory = scalax.collection.GraphEdge.DiEdge
+      g.addEdge(nodes(0).toInt,nodes(1).toInt)
+    }
+    if (! (allPeople contains nodes(0)) )
+      allPeople += nodes(0)
+    else if (! (allPeople contains nodes(1)) )
+      allPeople += nodes(1)
+  }
+
+  def getPoints(): Unit = {
+    println("Calculing the points for the people so far listed.")
+    for (person <- allPeople) {
+      val points = calcNodeValue(person, 0)
+      println("Person " + person + " get " + points + " point(s)")
+    }
+  }
 
   def calcNodeValue(person: String, weight: Int): Double = {
     var sum = 0.0
